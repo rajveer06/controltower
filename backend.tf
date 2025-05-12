a19519f980d03ed5bc73780579c5f73dc3f3c536
rajveer <rajveersinghchouha11@gmail.com>
2025-05-12 16:08:30 +0530

    code fix

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "your-existing-bucket-name"        
     key            = "control-tower/terraform.tfstate"   
-    region         = "ap-south-1"                          
-    use_lockfile   = true              
+    region         = "ap-south-1"                                        
   }
 }
